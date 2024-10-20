d955c43dafb9f47be41070fae2a8d787a4bf061d
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-10-20 23:01:01 +0600

    js

--- practice_register/app.js
+++ practice_register/app.js
@@ -1,43 +1,94 @@
 
+// let Name = document.getElementById("name");
+// let Phone = document.getElementById("Phone");
+// let Email = document.getElementById("Email");
+// let Password = document.getElementById("Password");
+// let SubmitBtn = document.getElementById("SubmitBtn");
+// let error = document.getElementById("error");
+
+// //Event listen
+
+// SubmitBtn.addEventListener("click", function(){
+//    if(Name.value == "" ||
+//      Phone.value == "" ||
+//      Email.value == "" ||
+//      Password.value == ""
+//     ){
+//    alert("Faile can be empty");
+//    return;
+//    };
+
+//    if(Name.value.length < 5 ){
+//     //alert('Name length should be more than 5 latters');
+//     error.innerHTML = "Name length should be more than 5 latters";
+//     return;
+//    }
+   
+//    if(Phone.value.length != 11 ){
+//     //alert('Phone length MUST BE 10 latters');
+//     error.innerHTML = "Name length must be 11 latters";
+
+//     return;
+  //  }
+   
+  //  if(Password.value.length < 8 ){
+  //   // alert('Password length should be more than 7 latters');
+  //   error.innerHTML = "Name length should be more than 7 latters";
+
+//     return;
+//    }
+// });
+
+
+
+
+
+
 let Name = document.getElementById("name");
-let Phone = document.getElementById("Phone");
-let Email = document.getElementById("Email");
-let Password = document.getElementById("Password");
-let SubmitBtn = document.getElementById("SubmitBtn");
-let error = document.getElementById("error");
+let phone = document.getElementById("Phone");
+let email = document.getElementById("Email");
+let password = document.getElementById("Password");
+let submitBtn = document.getElementById("Submit-btn");
+let errorTag = document.getElementById("error");
 
-//Event listen
-
-SubmitBtn.addEventListener("click", function(){
-   if(Name.value == "" ||
-     Phone.value == "" ||
-     Email.value == "" ||
-     Password.value == ""
-    ){
-   alert("Faile can be empty");
-   return;
-   };
-
-   if(Name.value.length < 5 ){
-    //alert('Name length should be more than 5 latters');
-    error.innerHTML = "Name length should be more than 5 latters";
+// event listen
+SubmitBtn.addEventListener("click", function () {
+  // Check blank fields
+  if (
+    Name.value == "" ||
+    phone.value == "" ||
+    email.value == "" ||
+    password.value == ""
+  ) {
+    // alert("Fields can't be empty!");
+    errorTag.innerHTML = "Fields can't be empty!";
     return;
-   }
-   
-   if(Phone.value.length != 11 ){
-    //alert('Phone length MUST BE 10 latters');
-    error.innerHTML = "Name length should be more than 5 latters";
+  }
 
+  // Name length check
+  if (Name.value.length < 5) {
+    //alert("Name length should be more than 5 latters.");
+    errorTag.innerHTML = "Name length should be more than 5 latters.";
     return;
-   }
-   
-   if(Password.value.length < 8 ){
-    //alert('Password length should be more than 7 latters');
-    error.innerHTML = "Name length should be more than 5 latters";
+  }
 
+  // Phone length check
+  if (phone.value.length != 11) {
+    //alert("Phone number must be in 11digits!");
+    errorTag.innerHTML = "Phone number must be in 11digits!";
     return;
-   }
+  }
+
+  // Password length check
+  if (password.value.length < 8) {
+    //alert("Password length should more than 7 char!");
+    errorTag.innerHTML = "Password length should more than 7 char!";
+    return;
+  }
+
+  // end event listener
 });
+
 
 
 
