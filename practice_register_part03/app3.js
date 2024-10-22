cb00913bc73e49b10b45c42e3b09484a04e66a02
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-10-22 20:08:55 +0600

    js

--- practice_register_part03/app3.js
+++ practice_register_part03/app3.js
@@ -7,9 +7,6 @@ let errorTag = document.querySelector("#errorTag");
 let submitBtne = document.querySelector("#submitBtn");
 
 
-let hideErrorTag = document.querySelector("#hideErrorTag"); 
-let showErrorTag = document.querySelector("#showErrorTag");
-
 
 // add event listener
 
@@ -19,22 +16,27 @@ submitBtne.addEventListener("click", function () {
     return;
   }
   if(Name.value.length < 5){
-    errorTag.innerHTML = "Your name must be more than 5 charactor";
+    //errorTag.innerHTML = "Your name must be more than 5 charactor";
+    showError("Your name must be more than 5 charactor");
     return;
   }
   if(phone.value.length != 11){
-    errorTag.innerHTML = "Your Number must be 11 charactor";
+   // errorTag.innerHTML = "Your Number must be 11 charactor";
+   showError("Your Number must be 11 charactor");
     return;
   }
   if(password.value.length < 8){
-    errorTag.innerHTML = "Your Number must be more than 8 charactor";
+   // errorTag.innerHTML = "Your Number must be more than 8 charactor";
+    showError("Your password must be more than 8 charactor");
     return;
   }
 });
 
 
 //function
-
+function showError(showText){
+  errorTag.innerHTML = showText;
+}
 function hideErrorTag(){
   errorTag.innerHTML = "";
 }
