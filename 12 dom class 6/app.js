99f02be1f24ed3bb45b8a28d8bd0483299eb9ded
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-10-25 16:58:49 +0600

    dom 12 class 6

---
+++ 12 dom class 6/app.js
@@ -0,0 +1,41 @@
+let postGrid = document.querySelector(".post-grid");
+let posts = [
+  {
+    title: "Post 1",
+    Author: " kamal",
+    Description: "this is post description",
+    like: 12,
+  },
+
+  {
+    title: "Post 2",
+    Author: " jamal",
+    Description: "this is post description",
+    like: 65,
+  },
+  {
+    title: "Post 3",
+    Author: " rahim",
+    Description: "this is post description",
+    like: 56,
+  },
+
+  {
+    title: "Post 4",
+    Author: " karim",
+    Description: "this is post description",
+    like: 92,
+  },
+];
+
+posts.forEach(function(post) {
+  let html = `<div class="poest-item">
+  <h1>${post.title}</h1>
+  <h4>${post.Author}</h4>
+  <p>${post.Description}</p>
+  <p>${post.like}</p>
+</div>`
+postGrid.innerHTML += html;
+});
+
+
