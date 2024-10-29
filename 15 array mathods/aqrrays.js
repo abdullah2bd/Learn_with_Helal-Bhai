49df1a0611f51b095dd6d34910668610e375f943
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-10-29 10:58:05 +0600

    array mathods

---
+++ 15 array mathods/aqrrays.js
@@ -0,0 +1,56 @@
+let mySubject = ["Quran", "Hadid", "Akaid", "Arabic 1st", "Arabic 2nd"];
+
+
+// mySubject.push("Bangla");
+// mySubject.shift();
+// mySubject.unshift("Math");
+// mySubject.pop();
+
+
+
+
+// let sub = mySubject.indexOf("Arabic 2nd");
+
+// console.log(mySubject.indexOf("Arabic 2nd"))
+
+
+
+
+
+// let sub = (mySubject.indexOf("Akaid"));
+// console.log(sub);
+
+// let sub = mySubject.splice(1,2, "www", "eee");
+
+
+
+let sub = mySubject.indexOf("Akaid");
+
+mySubject.slice(sub, 1);
+
+
+
+console.log(mySubject);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
