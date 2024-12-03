46208653baadd66c9d8d252eb5b79e91ff7ca300
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-12-03 08:02:05 +0600

    new

--- 11 register/register.js
+++ 11 register/register.js
@@ -8,96 +8,47 @@ let submitBtn = document.querySelector("#submitBtn");
 // all input
 let input = document.querySelectorAll("input");
 
-
-
-
-
-
 // add event
 
-submitBtn.addEventListener("click", function() {
- if(Name.value == "" || number.value == "" || email.value == "" || password.value == ""){
-  errorTag.innerHTML = "Please Chack";
-  return;
- };
-if(Name.value.length < 5){
-  errorTag.innerHTML = "Name must be more than 5 charactar";
-  return
- };
- 
- if(number.value.length != 11){
-  errorTag.innerHTML = "Number must be 11 charactar";
-  return
- };
- if(password.value.length < 8){
-  errorTag.innerHTML = "Password must be more than 8 charactar";
-  return
- };
+submitBtn.addEventListener("click", function () {
+  if (
+    Name.value == "" ||
+    number.value == "" ||
+    email.value == "" ||
+    password.value == ""
+  ) {
+    errorTag.innerHTML = "Please Chack";
+    return;
+  }
+  if (Name.value.length < 5) {
+    errorTag.innerHTML = "Name must be more than 5 charactar";
+    return;
+  }
+
+  if (number.value.length != 11) {
+    errorTag.innerHTML = "Number must be 11 charactar";
+    return;
+  }
+  if (password.value.length < 8) {
+    errorTag.innerHTML = "Password must be more than 8 charactar";
+    return;
+  }
 });
 
-
-
-
-
-
-
-
-// my function 
+// my function
 
 function showTag(show) {
   errorTag.innerHTML = show;
-};
+}
 
-function hide(){
+function hide() {
   errorTag.innerHTML = "";
-};
+}
 
-// addd key down 
+// addd key down
 
-input.forEach(function(item){
-  item.addEventListener('keydown', function(){
+input.forEach(function (item) {
+  item.addEventListener("keydown", function () {
     hide();
   });
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
