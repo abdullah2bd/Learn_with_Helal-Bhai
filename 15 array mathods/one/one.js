7c653b3b4dedb7b9e52a2b3db1c726b9eee2d314
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-12-03 15:57:50 +0600

    array in details

---
+++ 15 array mathods/one/one.js
@@ -0,0 +1,25 @@
+let mySubject = ["Quran", "Hadid", "Akaid", "Arabic 1st", "Arabic 2nd"];
+
+
+
+
+// mySubject.push("Bangla");
+// mySubject.shift(0,3);
+// mySubject.unshift("Math", "hhh");
+// mySubject.pop(2);
+
+// let sub = mySubject.indexOf("Arabic 2nd");
+
+// console.log(mySubject.indexOf("Arabic 2nd"))
+
+// let sub = (mySubject.indexOf("Akaid"));
+// console.log(sub);
+
+// let sub = mySubject.splice(3,4);
+
+// let sub = mySubject.indexOf("Akaid");
+
+// mySubject.slice(sub, 1);
+
+console.log(sub);
+
