fdc4a9f3513dd00180e51a9a96e2167d0bea30ac
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-12-04 11:44:20 +0600

    to do

--- 16 toDolist/todolist.js
+++ 16 toDolist/todolist.js
@@ -11,12 +11,28 @@ let todos = ["one", "two", "three", "four"];
 
 
 
+// add event listener
+
+
+addTaskBtn.addEventListener("click", function(){
+  todos.forEach(function(item){
+    todolist.innerHTML = `<li> ${todos}</li>`;
+    // addTask();
+  })
+})
+
+
+//add task
+
+function addTask(){
 
 
 
+  
 
 
 
+}
 
 
 
